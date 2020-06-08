c8b7d1dc6662ebce685bb5b802d17fcef96cc33f
Torje N Eikenes <torje.n.eikenes@gmail.com>
2020-06-08 21:57:10 +0200

    organisation

--- Pathfinder.cpp
+++ Pathfinder.cpp
@@ -49,20 +49,15 @@ Pathfinder::Pathfinder(int x, int y, int s,Loc start, Loc end,Vector<Loc> blck)
 }
 
 
-// Returns pointer to Cell with given loc
-Cell* Pathfinder::getCell(Loc l){
-    //Returns nullptr if outside of grid
-    try{
-        int i=l.y*xcell+l.x;
-        if(l.x>=xcell || l.x<0 || l.y >=ycell || l.y < 0){
-            throw range_error("Out of bounds");
-        }
-        return &vr[i];
-    }
-    catch (range_error& e){
-        //std::cerr<<e.what();
-        return nullptr;
-    }
+
+#pragma region dijkstra
+//starts the algorithm 
+void Pathfinder::strt() {
+    running = true;
+    dijkstra(0.05);
+    running = false;
+    // runs handleClicks again to keep window open after completion
+    handleClicks();
 }
 
 void Pathfinder::dijkstra(double waitTime=0){ 
@@ -103,26 +98,6 @@ void Pathfinder::dijkstra(double waitTime=0){
     flush();
 }
 
-// Draws path by tracing the parent pointer
-void Pathfinder::drawPath(Cell* start,Cell* end){
-    Cell* c = start;
-    // Runs until c is at the end
-    while(c!=end){
-        //Changes color to yellow 
-        c->set_fill_color(pathC);
-        //sets c to its own parrent
-        c=c->getParent();
-    }
-}
-
-//Handles click events while not running
-void Pathfinder::handleClicks(){
-    while(!running){
-        Fl::wait();
-        click();         
-    }
-}
-
 // Compares current cell with the cell with given offset and set distance
 void Pathfinder::compareCells(Cell* cur,int xOffset,int yOffset){
     int d = 10; // multiplier for minimum cost
@@ -155,202 +130,9 @@ Cell* Pathfinder::getMinDist(){
     return e;
 }
 
-void Pathfinder::click(){
-    Point xy{Fl::event_x(),Fl::event_y()};//Mouse position
-    MouseButton mb = static_cast<MouseButton>(Fl::event_button()); // Mouse button
+#pragma endregion
 
-    // If a mouse button is pressed and is inside grid
-    if(Fl::event_buttons()>0 && inRange(xy)){
-        // c is set to pointer to cell under mouse
-        auto c = getCell(pntToLoc(xy));
-
-        switch (mb)
-        {
-        case MouseButton::left:
-            //sets startPress and endPress to true for dragging
-            if(c->getLoc() == start){
-                startPress = true;
-            }
-            else if(c->getLoc() == end){
-                endPress = true;
-            }
-            // If startPress or endPress is true, it moves the start or end to 
-            // the new location of the mouse
-            else if(startPress){
-                setStart(c->getLoc());
-            }
-            else if(endPress){
-                setEnd(c->getLoc());
-            }
-            // Blocks cell under mouse
-            else{
-                c->setBlocked();
-            }
-            break;
-        case MouseButton::right:
-            // Empties cell under mouse
-            c->setEmpty();
-            break;
-        }
-        // reloads window
-        flush();
-    }
-    else{
-        //Stops dragging
-        startPress = false;
-        endPress = false;
-    }
-}
-
-//Changes start cell to the given location
-void Pathfinder::setStart(Loc l){
-    getCell(start)->setEmpty();
-    start=l;
-    getCell(start)->set_fill_color(startC);
-}
-
-//Changes end cell to the given location
-void Pathfinder::setEnd(Loc l){
-    getCell(end)->setEmpty();
-    end=l;
-    getCell(end)->set_fill_color(endC);
-}
-
-// Callback function from the start button
-void Pathfinder::cb_start(Address, Address addr){ 
-    static_cast<Pathfinder *>(addr)->dijkstra(0.05);
-}
-
-//starts the algorithm 
-void Pathfinder::strt() {
-    running = true;
-    dijkstra(0.05);
-    running = false;
-    // runs handleClicks again to keep window open after completion
-    handleClicks();
-}
-
-void Pathfinder::cb_clear(Address, Address addr){ 
-    static_cast<Pathfinder *>(addr)->clearBlk();
-}
-
-void Pathfinder::clear() {
-    //Set all searched cells to empty
-    for(auto c:searched){
-        c->setEmpty();
-    }
-    searched.clear();
-    //Color start and end
-    getCell(start)->set_fill_color(startC);
-    getCell(end)->set_fill_color(endC);
-
-    q.clear();
-    for(auto e:vr){
-        e->setDist(std::numeric_limits<int>::max());
-        e->setCost(std::numeric_limits<int>::max());
-        q.insert(e);
-    };
-    moveCtr = 0;
-    moves.set_label("Moves: 0");
-
-    //redraw window
-    redraw();
-}
-
-void Pathfinder::mazeGen(double waitTime=0){
-    clear();
-    Loc last = end;
-    setEnd(Loc(xcell-1,ycell-1));
-
-    // Set all cells to blocked
-    for(auto c:vr){
-        c->setBlocked();
-    }
-
-    stack<Cell*> st; // stack for visited cells
-    Cell* nxt; // Next cell
-    Cell* cur; // Current cell
-
-    st.push(getCell(start)); //Add first cell to the que stack
-    st.top()->setEmpty(); // set the first cell to empty
-    flush(); // Refresh window
-
-    while(st.size()>0){
-        cur = st.top(); // set the current cell to the cell of stack
-        st.pop(); // and delete it from the stack
-
-        // Vector with directions
-        vector<pair<int,int>> dir{make_pair(0,1),make_pair(1,0),
-                                  make_pair(0,-1),make_pair(-1,0)};
-
-        do{
-            // Choose a random direction and delete it from the direction vector
-            int i = rand() % dir.size(); 
-            auto d = dir[i];
-            dir.erase(dir.begin()+i);
-
-            // Try to open wall and cell in the chosen direction
-            nxt = openCell(cur,d.first,d.second);
-        }
-        // Runs until a direction is inside grid or no direction is possible
-        while(nxt == nullptr && dir.size()>0 ); 
-
-        // If a direction is possible
-        if(nxt != nullptr){
-            //add current and next position to the stack
-            st.push(cur);
-            st.push(nxt);
-
-            Fl::wait(waitTime);// wait
-
-            // Set cur cell to last cell 
-            last = nxt->getLoc();
-            flush(); 
-        }
-    }
-    // Set start and end to make them visible
-    setStart(start);
-    setEnd(last);
-    flush();
-}
-
-// Open wall and cell in given direction or return nullptr if not possible
-Cell* Pathfinder::openCell(Cell* cur,int xOffset,int yOffset){
-    // Wall cell is the cell next to the current cell in the given direction
-    auto wall = getCell(Loc{cur->getLoc().x+xOffset,cur->getLoc().y+yOffset});
-    // Checks if wall is inside grid and is blocked
-    if(wall!=nullptr && wall->getStatus()==Stat::blocked){
-        // Next cell is the cell next to the wall cell in the given direction
-        auto next = getCell(Loc{wall->getLoc().x+xOffset,wall->getLoc().y+yOffset});
-        // Checks if next cell is inside grid and is blocked
-        if(next!=nullptr && next->getStatus()==Stat::blocked){
-            // set wall and cell to empty 
-            wall->setEmpty();
-            next->setEmpty();
-            return next;
-        }
-        else{
-            // Return nullptr if cell is outside grid or not blocked
-            return nullptr; 
-        }
-    }
-    else
-    {
-        // Return nullptr if wall is outside grid or not blocked
-        return nullptr;
-    }
-}
-
-// Maze button callback
-void Pathfinder::cb_maze(Address, Address addr){
-    static_cast<Pathfinder *>(addr)->mazeGen(0.05);
-}
-
-// Astar button callback
-void Pathfinder::cb_ast(Address, Address addr){
-    static_cast<Pathfinder *>(addr)->aStar(0.05);
-}
-
+#pragma region a-star
 void Pathfinder::aStar(double waitTime=0){ 
     clear();
     // the current cell is set to the start location
@@ -439,12 +221,248 @@ void Pathfinder::compareCellsAst(Cell* cur,int xOffset,int yOffset){
     }
 }
 
+#pragma endregion
+
+// Draws path by tracing the parent pointer
+void Pathfinder::drawPath(Cell* start,Cell* end){
+    Cell* c = start;
+    // Runs until c is at the end
+    while(c!=end){
+        //Changes color to yellow 
+        c->set_fill_color(pathC);
+        //sets c to its own parrent
+        c=c->getParent();
+    }
+}
+
+#pragma region maze
+void Pathfinder::mazeGen(double waitTime=0){
+    clear();
+    Loc last = end;
+    setEnd(Loc(xcell-1,ycell-1));
+
+    // Set all cells to blocked
+    for(auto c:vr){
+        c->setBlocked();
+    }
+
+    stack<Cell*> st; // stack for visited cells
+    Cell* nxt; // Next cell
+    Cell* cur; // Current cell
+
+    st.push(getCell(start)); //Add first cell to the que stack
+    st.top()->setEmpty(); // set the first cell to empty
+    flush(); // Refresh window
+
+    while(st.size()>0){
+        cur = st.top(); // set the current cell to the cell of stack
+        st.pop(); // and delete it from the stack
+
+        // Vector with directions
+        vector<pair<int,int>> dir{make_pair(0,1),make_pair(1,0),
+                                  make_pair(0,-1),make_pair(-1,0)};
+
+        do{
+            // Choose a random direction and delete it from the direction vector
+            int i = rand() % dir.size(); 
+            auto d = dir[i];
+            dir.erase(dir.begin()+i);
+
+            // Try to open wall and cell in the chosen direction
+            nxt = openCell(cur,d.first,d.second);
+        }
+        // Runs until a direction is inside grid or no direction is possible
+        while(nxt == nullptr && dir.size()>0 ); 
+
+        // If a direction is possible
+        if(nxt != nullptr){
+            //add current and next position to the stack
+            st.push(cur);
+            st.push(nxt);
+
+            Fl::wait(waitTime);// wait
+
+            // Set cur cell to last cell 
+            last = nxt->getLoc();
+            flush(); 
+        }
+    }
+    // Set start and end to make them visible
+    setStart(start);
+    setEnd(last);
+    flush();
+}
+
+// Open wall and cell in given direction or return nullptr if not possible
+Cell* Pathfinder::openCell(Cell* cur,int xOffset,int yOffset){
+    // Wall cell is the cell next to the current cell in the given direction
+    auto wall = getCell(Loc{cur->getLoc().x+xOffset,cur->getLoc().y+yOffset});
+    // Checks if wall is inside grid and is blocked
+    if(wall!=nullptr && wall->getStatus()==Stat::blocked){
+        // Next cell is the cell next to the wall cell in the given direction
+        auto next = getCell(Loc{wall->getLoc().x+xOffset,wall->getLoc().y+yOffset});
+        // Checks if next cell is inside grid and is blocked
+        if(next!=nullptr && next->getStatus()==Stat::blocked){
+            // set wall and cell to empty 
+            wall->setEmpty();
+            next->setEmpty();
+            return next;
+        }
+        else{
+            // Return nullptr if cell is outside grid or not blocked
+            return nullptr; 
+        }
+    }
+    else
+    {
+        // Return nullptr if wall is outside grid or not blocked
+        return nullptr;
+    }
+}
+#pragma endregion
+
+#pragma region clicks
+//Handles click events while not running
+void Pathfinder::handleClicks(){
+    while(!running){
+        Fl::wait();
+        click();         
+    }
+}
+
+void Pathfinder::click(){
+    Point xy{Fl::event_x(),Fl::event_y()};//Mouse position
+    MouseButton mb = static_cast<MouseButton>(Fl::event_button()); // Mouse button
+
+    // If a mouse button is pressed and is inside grid
+    if(Fl::event_buttons()>0 && inRange(xy)){
+        // c is set to pointer to cell under mouse
+        auto c = getCell(pntToLoc(xy));
+
+        switch (mb)
+        {
+        case MouseButton::left:
+            //sets startPress and endPress to true for dragging
+            if(c->getLoc() == start){
+                startPress = true;
+            }
+            else if(c->getLoc() == end){
+                endPress = true;
+            }
+            // If startPress or endPress is true, it moves the start or end to 
+            // the new location of the mouse
+            else if(startPress){
+                setStart(c->getLoc());
+            }
+            else if(endPress){
+                setEnd(c->getLoc());
+            }
+            // Blocks cell under mouse
+            else{
+                c->setBlocked();
+            }
+            break;
+        case MouseButton::right:
+            // Empties cell under mouse
+            c->setEmpty();
+            break;
+        }
+        // reloads window
+        flush();
+    }
+    else{
+        //Stops dragging
+        startPress = false;
+        endPress = false;
+    }
+}
+
+#pragma endregion
+
+#pragma region set-get
+//Changes start cell to the given location
+void Pathfinder::setStart(Loc l){
+    getCell(start)->setEmpty();
+    start=l;
+    getCell(start)->set_fill_color(startC);
+}
+
+//Changes end cell to the given location
+void Pathfinder::setEnd(Loc l){
+    getCell(end)->setEmpty();
+    end=l;
+    getCell(end)->set_fill_color(endC);
+}
+
+// Returns pointer to Cell with given loc
+Cell* Pathfinder::getCell(Loc l){
+    //Returns nullptr if outside of grid
+    try{
+        int i=l.y*xcell+l.x;
+        if(l.x>=xcell || l.x<0 || l.y >=ycell || l.y < 0){
+            throw range_error("Out of bounds");
+        }
+        return &vr[i];
+    }
+    catch (range_error& e){
+        //std::cerr<<e.what();
+        return nullptr;
+    }
+}
+
+#pragma endregion
+
+#pragma region callback
+// Maze button callback
+void Pathfinder::cb_maze(Address, Address addr){
+    static_cast<Pathfinder *>(addr)->mazeGen(0.05);
+}
+
+// Astar button callback
+void Pathfinder::cb_ast(Address, Address addr){
+    static_cast<Pathfinder *>(addr)->aStar(0.05);
+}
+
+// Callback function from the start button
+void Pathfinder::cb_start(Address, Address addr){ 
+    static_cast<Pathfinder *>(addr)->dijkstra(0.05);
+}
+
+void Pathfinder::cb_clear(Address, Address addr){ 
+    static_cast<Pathfinder *>(addr)->clearBlk();
+}
+#pragma endregion
+
 // Increment the move counter
 void Pathfinder::addMove(){
     moveCtr++;
     moves.set_label("Moves: " + to_string(moveCtr));
 }
 
+#pragma region clear
+// Clear all searched cells. Not blocked
+void Pathfinder::clear() {
+    //Set all searched cells to empty
+    for(auto c:searched){
+        c->setEmpty();
+    }
+    searched.clear();
+    //Color start and end
+    getCell(start)->set_fill_color(startC);
+    getCell(end)->set_fill_color(endC);
+
+    q.clear();
+    for(auto e:vr){
+        e->setDist(std::numeric_limits<int>::max());
+        e->setCost(std::numeric_limits<int>::max());
+        q.insert(e);
+    };
+    moveCtr = 0;
+    moves.set_label("Moves: 0");
+
+    //redraw window
+    redraw();
+}
 // Clear all cells including blocked
 void Pathfinder::clearBlk(){
     //Set all searched cells to empty
@@ -468,3 +486,4 @@ void Pathfinder::clearBlk(){
     //redraw window
     redraw();
 }
+#pragma endregion
--- Pathfinder.h
+++ Pathfinder.h
@@ -10,66 +10,74 @@
 enum class MouseButton { left = FL_LEFT_MOUSE, right = FL_RIGHT_MOUSE};
 
 class Pathfinder : public Graph_lib::Window{
+public:
+    Pathfinder(int x, int y, int s,Loc start, Loc end,Vector<Loc> blck=Vector<Loc>{0});
+
+    void dijkstra(double waitTime);
+    void aStar(double waitTime);
+    void mazeGen(double waitTime);
+
+    //clicks
+    void handleClicks();
+
+    //clear
+    void clearBlk();
+    void clear();
+
+    Button dijkstraBt, AstBt, mazeBt, clearBt;
+    Text moves;
+    Rectangle mvBg;
 private:
     int xcell, ycell, cellSize;
     Vector_ref<Cell> vr;
     Loc start;
     Loc end;
     Vector<Loc> blocked;
-    bool running;
-    bool startPress;
-    bool endPress;
+    bool running, startPress, endPress;
     Vector<Cell*> route;
     set<Cell*> q;
     Vector<Cell*> searched;
     int moveCtr;
-    Color searchC;
-    Color pathC; 
-    Color startC;
-    Color endC;
+    Color searchC, pathC, startC, endC;
 
+    // Dijkstra
     void strt();
     void compareCells(Cell* cur,int xOffset,int yOffset);
     Cell* getMinDist();
 
+    // A*
     int manhattan(Cell* c);
     void compareCellsAst(Cell* cur,int xOffset,int yOffset);
     Cell* getMinCost();
 
+    void drawPath(Cell* start,Cell* end);
+
+    // Maze
     Cell* openCell(Cell* cur,int xOffset,int yOffset);
 
-    void clear();
+
+    //clicks
     void click();
+
+    //set get
+    void setStart(Loc l);
+    void setEnd(Loc l);
+    Cell* getCell(Loc l);
+
+    // Callback
     static void cb_start(Address, Address addr);
     static void cb_clear(Address, Address addr);
     static void cb_maze(Address, Address addr);
     static void cb_ast(Address, Address addr);
 
+
     void addMove();
-    void setStart(Loc l);
-    void setEnd(Loc l);
+
+    
     int Width() const { return xcell*cellSize;}
     int Height() const { return ycell*cellSize;}
     bool inRange(Point xy) const { return xy.x >= 0 && xy.x < Width() && xy.y >= 0 && xy.y < Height(); }
     Loc pntToLoc(Point xy) {return Loc{xy.x/cellSize,xy.y/cellSize};}
-public:
-    Pathfinder(int x, int y, int s,Loc start, Loc end,Vector<Loc> blck=Vector<Loc>{0});
-    Cell* getCell(Loc l);
-
-    void dijkstra(double waitTime);
-    void aStar(double waitTime);
-    void mazeGen(double waitTime);
-
-    void drawPath(Cell* start,Cell* end);
-    void handleClicks();
-    void clearBlk();
-
-    Button dijkstraBt;
-    Button AstBt;
-    Button mazeBt;
-    Button clearBt;
-    Text moves;
-    Rectangle mvBg;
 };
 
 
